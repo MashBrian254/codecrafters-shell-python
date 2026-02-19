ad8aa2f94dd6e48216ab010122cfd129ed25611c
Mash <brian.macharia731@gmail.com>
2026-02-19 13:57:04 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,16 +5,29 @@ def main():
 
     while True:
         sys.stdout.write("$ ")
+        builtins =['type','exit','echo']
+
         command = input()
-        if command == "exit":
+        
+        s = command.split()
+        if s[0] == "exit":
             break
 
-        s = command.split()
-
         if len(s) > 0:
+            
             if s[0] == "echo":
                 text = " ".join(s[1:])
                 print(f"{text}")
+        
+            elif s[0] == 'type':
+                if len(s) > 1:
+                    if s[1] in builtins:
+                        print(f"{s[1]} is a shell builtin")
+                    else:
+                        print(f"{s[1]}: command not found")
+                else:
+                    print("please enter a command")
+
             else:
                 print(f"{s[0]}: command not found")
 
